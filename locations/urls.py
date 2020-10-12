b8a7f0f8a8261e4670c734a480f3d0ebc91cb66d
Lennox <omondilenox@outlook.com>
2020-10-12 12:20:50 +0300

    changes add arg to locaiton.shpw

--- locations/urls.py
+++ locations/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from locations import views
 urlpatterns = [
     path('',views.index,name="locations.index"),
-    path('<int:category_id>', views.show, name="locations.show")
+    path('<int:location_id>', views.show, name="locations.show")
 
 ]
