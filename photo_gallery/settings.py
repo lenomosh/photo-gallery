a92610f23dfff546652f36b8bf4d09963a520ea5
Lennox <omondilenox@outlook.com>
2020-10-13 16:13:04 +0300

    hmtl files updated

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,10 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+]
--- photo_gallery/static/js/main.js
+++ photo_gallery/static/js/main.js
@@ -112,40 +112,6 @@ jQuery(document).ready(function ($) {
         });
 
 
-        setTimeout(function () {
-
-            var counter = 0;
-            $('.site-mobile-menu .has-children').each(function () {
-                var $this = $(this);
-
-                $this.prepend('<span class="arrow-collapse collapsed">');
-
-                $this.find('.arrow-collapse').attr({
-                    'data-toggle': 'collapse',
-                    'data-target': '#collapseItem' + counter,
-                });
-
-                $this.find('> ul').attr({
-                    'class': 'collapse',
-                    'id': 'collapseItem' + counter,
-                });
-
-                counter++;
-
-            });
-
-        }, 1000);
-
-        $('body').on('click', '.arrow-collapse', function (e) {
-            var $this = $(this);
-            if ($this.closest('li').find('.collapse').hasClass('show')) {
-                $this.removeClass('active');
-            } else {
-                $this.addClass('active');
-            }
-            e.preventDefault();
-
-        });
 
         $(window).resize(function () {
             var $this = $(this),
@@ -170,8 +136,6 @@ jQuery(document).ready(function ($) {
                 $this.addClass('active');
             }
         })
-
-        // click outisde offcanvas
         $(document).mouseup(function (e) {
             var container = $(".site-mobile-menu");
             if (!container.is(e.target) && container.has(e.target).length === 0) {
