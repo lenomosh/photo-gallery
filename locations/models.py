3ed143d222b752fdbd7f6575039e8167c095faf2
Lennox <omondilenox@outlook.com>
2020-10-13 08:11:24 +0300

    updated locations model

---
+++ locations/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+Charfield = models.CharField,
+
+
+# Create your models here.
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def save_location(self) -> None:
+        self.save()
+
+    def update_location(self, name) -> None:
+        self.name = name
+        self.save()
+
+    def delete_location(self) -> None:
+        self.delete()
