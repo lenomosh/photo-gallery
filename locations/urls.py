74fe23923933f3084865198ced5f58e32db072fd
Lennox <omondilenox@outlook.com>
2020-10-13 08:47:49 +0300

    updated locations urls.py

--- locations/urls.py
+++ locations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from locations import views
 urlpatterns = [
-    path('',views.index,name="locations.index"),
-    path('<int:location_id>', views.show, name="locations.show")
+    path('', views.index, name="locations.index"),
+    path('<int:location_id>', views.show, name="locations.show"),
+    path('get_locations', views.get_all_locations, name="locations.all")
 
 ]
