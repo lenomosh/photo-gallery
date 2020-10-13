8e58ed3b10927c1831f6a89e1e16ca16c66bbdbf
Lennox <omondilenox@outlook.com>
2020-10-13 08:50:01 +0300

    added images url to main urls.py

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('pages.urls')),
-    path('admin/', admin.site.urls),
-]
+                  path('', include('pages.urls')),
+                  path('images', include('images.urls')),
+                  path('categories/', include('categories.urls')),
+                  path('locations/', include('locations.urls')),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
